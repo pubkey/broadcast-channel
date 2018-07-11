04f310c3289a13ff5c6b3f40a001ca08478d76df
pubkey <daniel_van_meyer@hotmail.de>
2018-07-11 18:50:49 +0200

    CHANGE(e2e) better output to debug travis

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -17,20 +17,19 @@ fixture`Example page`
     'default'
 ].forEach(methodType => {
     test.page(BASE_PAGE + '?methodType=' + methodType + '&autoStart=startBroadcastChannel')('test(BroadcastChannel) with method: ' + methodType, async () => {
-        console.log('##### START TEST WITH ' + methodType);
+        console.log('##### START BroadcastChannel TEST WITH ' + methodType);
         const stateContainer = Selector('#state');
 
         await AsyncTestUtil.waitUntil(async () => {
-
-            // make a console.log so travis does not terminate because of no output
-            console.log('.');
-
             const value = await stateContainer.innerText;
             //       console.log(value);
 
             // const out = await t.getBrowserConsoleMessages();
             // console.log('out:');
             // console.log(JSON.stringify(out));
+
+            // make a console.log so travis does not terminate because of no output
+            console.log('BroadcastChannel(' + methodType + ') still no success');
 
             return value === 'SUCCESS';
         }, 0, 500);
@@ -46,20 +45,19 @@ fixture`Example page`
     'default'
 ].forEach(methodType => {
     test.page(BASE_PAGE + '?methodType=' + methodType + '&autoStart=startLeaderElection')('test(LeaderElection) with method: ' + methodType, async () => {
-        console.log('##### START TEST WITH ' + methodType);
+        console.log('##### START LeaderElection TEST WITH ' + methodType);
         const stateContainer = Selector('#state');
 
         await AsyncTestUtil.waitUntil(async () => {
-
-            // make a console.log so travis does not terminate because of no output
-            console.log('.');
-
             const value = await stateContainer.innerText;
             //       console.log(value);
 
             // const out = await t.getBrowserConsoleMessages();
             // console.log('out:');
             // console.log(JSON.stringify(out));
+
+            // make a console.log so travis does not terminate because of no output
+                console.log('LeaderElection(' + methodType + ') still no success');
 
             return value === 'SUCCESS';
         }, 0, 1000);
