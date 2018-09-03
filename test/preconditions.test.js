94c8922fb9bb31a94b1c15d588988b5dac0c1004
pubkey <daniel_van_meyer@hotmail.de>
2018-09-03 21:30:56 +0200

    CHORE FIX EADDRINUSE on OSX 10

--- test/preconditions.test.js
+++ test/preconditions.test.js
@@ -9,16 +9,26 @@ const isNode = require('detect-node');
 const path = require('path');
 const os = require('os');
 const net = require('net');
+const fs = require('fs');
+const util = require('util');
+
+const mkdir = util.promisify(fs.mkdir);
 
 describe('preconditions.test.js', () => {
     // does not work on OSX for whatever reason
     it('listen to multiple sockets at the same time', async () => {
         if (!isNode) return;
-        const folder = os.tmpdir();
+
+        // create sub-folder in tmp
+        const basePath = path.join(
+            os.tmpdir(),
+            'bc-precondition-sockets'
+        );
+        await mkdir(basePath).catch(() => null);
 
         // create server 1
         const server1Path = path.join(
-            folder,
+            basePath,
             'test1.s'
         );
         console.log('server1Path: ' + server1Path);
@@ -49,7 +59,7 @@ describe('preconditions.test.js', () => {
 
         // create server 2
         const server2Path = path.join(
-            folder,
+            basePath,
             'test2.s'
         );
         console.log('server2Path: ' + server2Path);
