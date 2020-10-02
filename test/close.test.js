e0cb4e56672cb45d51ab84625259d0ab8e81ba1a
Ariel Nava <arielnava@performityinc.com>
2020-10-02 08:26:02 -0700

    Added Jest test and tweaked babel config a bit to make it work #348
    Rebased test commit to include new code
    
    Also added eslint jest env so it doesn't complain about missing symbols

---
+++ test/close.test.js
@@ -0,0 +1,32 @@
+import BroadcastChannel from 'broadcast-channel';
+
+class Foo {
+    constructor () {
+        this.bc = new BroadcastChannel.BroadcastChannel('test');
+        this.bc.addEventListener('message', this.cb);
+    }
+
+    cb () {}
+}
+
+
+describe('Broadcast Channel', () => {
+    beforeEach(async () => {
+        await BroadcastChannel.clearNodeFolder();
+    });
+
+    test('local', async () => {
+        const foo = new Foo();
+
+        const result = await new Promise((a) => {
+            setTimeout(() => {
+                a(true);
+            }, 1000);
+        });
+
+        expect(result).toBe(true);
+
+        // Cleanup
+        await foo.bc.close();
+    });
+});
