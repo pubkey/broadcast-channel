a7779f89c97c5ce9b9eba324a027f947a900da0a
chaitanyapotti <chaitanya.potti@gmail.com>
2022-05-10 17:22:43 +0800

    add server to types

--- types/broadcast-channel.d.ts
+++ types/broadcast-channel.d.ts
@@ -1,4 +1,4 @@
-declare type MethodType = 'node' | 'idb' | 'native' | 'localstorage' | 'simulate';
+declare type MethodType = 'node' | 'idb' | 'native' | 'localstorage' | 'simulate' | 'server';
 
 interface BroadcastChannelEventMap {
     message: MessageEvent;
